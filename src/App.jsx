c8ecc108cb64776656ea04203ae69e9ca6c86b66
Mohd Zubair Alam <zubairalambpl8@gmail.com>
2025-05-17 02:15:50 +0530

    add my name is App.jsx , second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import "remixicon/fonts/remixicon.css";
 import Input from "./components/Input";
 function App() {
   return (
-    <div className="h-screen w-screen bg-slate-800 flex justify-center py-15">
+    <div className="h-screen w-screen bg-slate-800 flex flex-col items-center gap-2 justify-start py-15">
+      <h1 className="text-4xl font-bold text-green-200 font-mono ">
+        Zubair's Creations
+      </h1>
       <div className="h-120 w-160 shadow-xl rounded-2xl bg-linear-to-t from-gray-500 to-zinc-800 p-5 flex flex-col gap-5 items-center border-white ">
         <Input />
       </div>
